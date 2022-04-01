028f32e72024a18d3658b0355588b23f0c730599
Alvaro Ducuara <alvaro.isledier@gmail.com>
2022-03-31 22:03:01 -0500

    se ha creado un nuevo runner para el carrito de compras (AddToCart)

--- src/main/java/co/com/client/webproject/test/runners/AgregarCompraCarritoTest.java
+++ src/main/java/co/com/client/webproject/test/runners/AgregarCompraCarritoTest.java
@@ -4,7 +4,7 @@ package co.com.client.webproject.test.runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-/*
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         snippets = CucumberOptions.SnippetType.CAMELCASE,
@@ -12,6 +12,6 @@ import org.junit.runner.RunWith;
         glue = {"co.com.client.webproject.test.stepdefinition"},
         tags = "",
         publish = true
-)*/
+)
 public class AgregarCompraCarritoTest {
 }
